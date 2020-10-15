31a4673f39c78875347e4b9805f30b446e1598e7
Pietro Di Bello <pierodibello@gmail.com>
2020-10-15 14:17:45 +0200

    export with no orders

--- Java/src/test/java/codingdojo/XMLExporterTest.java
+++ Java/src/test/java/codingdojo/XMLExporterTest.java
@@ -1,14 +1,16 @@
 package codingdojo;
 
+import org.approvaltests.Approvals;
 import org.junit.Test;
+
+import static java.util.Collections.emptyList;
 
 public class XMLExporterTest {
 
     @Test
-    public void testSomething() {
-
-        // TODO: implement this test
-
+    public void export_no_orders() {
+        String result = XMLExporter.exportFull(emptyList());
+        Approvals.verify(result);
     }
 
 }
