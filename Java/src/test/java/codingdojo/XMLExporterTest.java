83884a3d7bbf0e9769f6143360e51d1c7088df43
Pietro Di Bello <pierodibello@gmail.com>
2020-10-15 14:38:34 +0200

    export with one order containing just one no-event product

--- Java/src/test/java/codingdojo/XMLExporterTest.java
+++ Java/src/test/java/codingdojo/XMLExporterTest.java
@@ -3,7 +3,7 @@ package codingdojo;
 import org.approvaltests.Approvals;
 import org.junit.Test;
 
-import static codingdojo.SampleModelObjects.RecentOrder;
+import static codingdojo.SampleModelObjects.*;
 import static java.util.Collections.emptyList;
 import static java.util.Collections.singletonList;
 
@@ -12,12 +12,25 @@ public class XMLExporterTest {
     @Test
     public void export_no_orders() {
         String result = XMLExporter.exportFull(emptyList());
+
         Approvals.verifyXml(result);
     }
 
     @Test
     public void export_with_one_order() {
         String result = XMLExporter.exportFull(singletonList(RecentOrder));
+
         Approvals.verifyXml(result);
     }
+
+    @Test
+    public void export_with_one_order_containing_just_one_no_event_product() {
+        Product noEventProduct = CherryBloom;
+        Order order = new Order("anyId", Util.fromIsoDate("2018-09-01T00:00Z"), FlagshipStore, new Product[]{noEventProduct});
+        String result = XMLExporter.exportFull(singletonList(order));
+
+        Approvals.verifyXml(result);
+    }
+
+
 }
